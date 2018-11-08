18816836449fbeaae45c97a8f5239c3b926632c2
Kyle Hanson <me@khanson.io>
2018-11-08 16:48:26 -0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -6,9 +6,13 @@ defmodule Cables.MixProject do
       app: :cables,
       version: "0.1.0",
       elixir: "~> 1.7",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      package: package(),
+      name: "Cables",
+      source_url: "https://github.com/hansonkd/cables"
     ]
   end
 
@@ -33,6 +37,16 @@ defmodule Cables.MixProject do
     [
       source_url: "https://github.com/hansonkd/cables",
       extras: ["README.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hansonkd/cables"}
     ]
   end
 end
